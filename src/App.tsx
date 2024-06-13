e4e6ef6e4d449fcb42ba229ef57d4e1e9da03c13
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-06-13 14:07:22 +0100

    Added and styled the user links div

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react';
 import moon from "./moon.svg"
 import search from "./search.svg"
 import axios from 'axios'
-
+import locaction from  "./location.svg"
+import website from "./website.svg"
+import twitter from "./twitter.svg"
+import company from "./company.svg"
 import './App.css';
 
 interface UserObject {
@@ -13,7 +16,12 @@ interface UserObject {
   html_url: string,
   public_repos: string,
   followers: string,
-  following: string
+  following: string,
+  location: string,
+  blog: string,
+  company: string,
+  twitter_username: string,
+
 }
 
 function App() {
@@ -42,7 +50,7 @@ function App() {
     setInputValue(e.target.value)
   }
   return (
-    <div className="App bg-[#f5f7ff] py-16">
+    <div className="App bg-[#f5f7ff] py-11 h-screen">
       <div className="flex-col gap-4 relative w-1/2 mx-auto">
         <header className='flex justify-between transition-all ease-in-out duration-500'>
           <h1 className='text-[#4B699B] text-[2.2rem] font-bold'>  devfinder</h1>
@@ -52,7 +60,7 @@ function App() {
           </div>
         </header>
         {/* <p className='absolute text-red-500 right-0 top-[2.1rem] font-bold'>User not found</p> */}
-        <div className='search-bar-div my-5 flex items-center p-2 rounded-2xl bg-white transition-all ease-in-out duration-500'>
+        <div className='search-bar-div my-4 flex items-center p-2 rounded-2xl bg-white transition-all ease-in-out duration-500'>
           <div className='my-0 mx-6'>
             <img className='w-8 h-6' src={search} alt='search icon' />
           </div>
@@ -66,13 +74,13 @@ function App() {
 
         {/* user details */}
         {userDetails &&
-          <div className='flex gap-8 p-8 rounded-2xl bg-white transition-all ease-in-out duration-500 '>
+          <div className='user-details-div flex gap-8 w-full p-8 rounded-2xl bg-white transition-all ease-in-out duration-500 '>
             <div>
               <img src={userDetails.avatar_url} className='max-h-[7.3125rem] rounded-full' alt='user' />
             </div>
 
             {/* Users profile data */}
-            <div>
+            <div className='w-full'>
               <div className='gap-8 flex w-full'>
                 <div className='name flex justify-between w-full'>
                   <div>
@@ -99,6 +107,30 @@ function App() {
                 <div>
                   <p className='text-[#4b699b] text-base'>Following</p>
                   <p className='mt-[0.635rem] font-bold text-[22px] text-[#2a3341]'>{userDetails.following}</p>
+                </div>
+              </div>
+
+              {/* User Links */}
+              <div className='flex justify-between w-full'>
+                <div className='w-[48%] cursor-pointer text-[#4B699B] text-base'>
+                  <div className='user-loc mb-4 flex items-start gap-4 break-all '>
+                    <img src={locaction} alt='location icon' />
+                    <p>{userDetails.location}</p>
+                  </div>
+                  <div className='user-loc mb-4 flex items-start gap-4 break-all'>
+                    <img src={website} alt='website icon' />
+                    <p>{userDetails.blog }</p>
+                  </div>
+                </div>
+                <div className='w-[48%] cursor-pointer text-[#4B699B] text-base'>
+                <div className='user-loc mb-4 flex items-start gap-4 break-all'>
+                <img src={twitter} alt='twitter icon' />
+                <p>{userDetails.twitter_username ? userDetails.twitter_username : 'Not Available'}</p>
+                </div>
+                <div className='user-loc mb-4 flex items-start gap-4 break-all'>
+                <img src={company} alt='company icon' />
+                <p>{userDetails.company}</p>
+                </div>
                 </div>
               </div>
             </div>
